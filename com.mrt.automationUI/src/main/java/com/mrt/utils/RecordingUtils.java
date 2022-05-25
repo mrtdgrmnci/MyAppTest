b7f6d5809c8b34dad46620efead64ad807d80e18
mrtdgrmnci <dadas4ever3232@gmail.com>
2022-05-25 18:39:22 -0400

    Recording utils import

--- com.mrt.automationUI/src/main/java/com/mrt/utils/RecordingUtils.java
+++ com.mrt.automationUI/src/main/java/com/mrt/utils/RecordingUtils.java
@@ -1,4 +1,16 @@
 package com.mrt.utils;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.monte.media.*;
+import org.monte.screenrecorder.ScreenRecorder;
+import static org.monte.media.AudioFormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
+
+/**
+ * @author Murat.Degirmenci
+ */
 public class RecordingUtils {
 }
